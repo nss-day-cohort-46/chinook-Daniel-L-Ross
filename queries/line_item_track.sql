90c17843568dc44dc3f2fd561abe2312e206a4ac
Dan Ross <daniellross@gmail.com>
2021-05-24 15:08:03 -0500

    finished 12, fixed #3

--- queries/line_item_track.sql
+++ queries/line_item_track.sql
@@ -1,3 +1,6 @@
 -- 12. line_item_track.sql: Provide a query that includes the purchased track name with each invoice line item.
 
-SELECT * FROM InvoiceLine
+SELECT i.*, t.name 
+FROM InvoiceLine i
+INNER JOIN track t
+WHERE t.trackId = i.trackId
