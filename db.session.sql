704158c36e687618da2af07ce80179e86a5272b8
Dan Ross <daniellross@gmail.com>
2021-05-24 14:30:07 -0500

    q9

--- db.session.sql
+++ db.session.sql
@@ -71,7 +71,13 @@ OR InvoiceDate like"%2011%"
 GROUP BY InvoiceYear
 
 -- 9. total_sales_{year}.sql: What are the respective total sales for each of those years?
+SELECT Total FROM Invoice
 
+SELECT strftime('%Y', InvoiceDate) AS InvoiceYear, SUM(Total)
+FROM Invoice
+WHERE InvoiceDate like"%2009%"
+OR InvoiceDate like"%2011%"
+GROUP BY InvoiceYear
 
 -- 10. invoice_37_line_item_count.sql: Looking at the InvoiceLine table, 
         -- provide a query that COUNTs the number of line items for Invoice ID 37.
