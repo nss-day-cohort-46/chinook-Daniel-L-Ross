8fbb747399afbd7f368778c646fefea07eb18961
Dan Ross <daniellross@gmail.com>
2021-05-24 15:36:21 -0500

    had to look at someone else code to see where my errors where on q 13

--- queries/line_item_track_artist.sql
+++ queries/line_item_track_artist.sql
@@ -1,10 +1,13 @@
 -- 13. line_item_track_artist.sql: Provide a query that includes the purchased track name AND artist name with each invoice line item.
 
-SELECT i.*, t.name
+SELECT 
+    t.Name AS TrackName,
+    ar.Name AS ArtistName,
+    i.*
 FROM InvoiceLine i
-INNER JOIN track t
-WHERE t.trackId = i.trackId
-JOIN Album al
-ON al.albumId = t.albumId
-JOIN Artist ar
-ON ar.artistId = al.artistId
+INNER JOIN Track t
+    ON i.trackId = t.trackId
+INNER JOIN Album al
+    ON al.albumId = t.albumId
+INNER JOIN Artist ar
+    ON ar.artistId = al.artistId
