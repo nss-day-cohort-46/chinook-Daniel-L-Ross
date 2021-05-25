f3d1bc40cd48d8e06cb69a0f01b1bd5f0818e407
Dan Ross <daniellross@gmail.com>
2021-05-25 14:23:34 -0500

    one moregaa

--- queries/top_3_artists.sql
+++ queries/top_3_artists.sql
@@ -2,4 +2,15 @@
 
 SELECT
     a.name,
+    COUNT(*) as tracksSold
+FROM Artist a
+INNER JOIN album on album.artistId = a.artistId
+INNER JOIN track on track.albumId = album.albumId
+INNER JOIN invoiceLine on invoiceLine.trackid = track.trackid
+GROUP BY a.artistId
+ORDER BY tracksSold DESC
+LIMIT 3
     
+
+
+artist -> album -> track -> invoiceLine
