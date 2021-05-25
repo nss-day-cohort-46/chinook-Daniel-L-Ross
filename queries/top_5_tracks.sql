0e4096912e710951b02c4b0289fda1231f683656
Dan Ross <daniellross@gmail.com>
2021-05-25 10:59:03 -0500

    25

---
+++ queries/top_5_tracks.sql
@@ -0,0 +1,11 @@
+-- 25. top_5_tracks.sql: Provide a query that shows the top 5 most purchased tracks over all.
+
+SELECT 
+    t.name as trackName, 
+    COUNT(il.trackId) as purchaseCount
+FROM InvoiceLine il
+INNER JOIN track t ON t.trackid = il.trackid
+INNER JOIN invoice i on i.invoiceId = il.invoiceId
+GROUP BY il.trackId
+ORDER BY purchaseCount DESC
+LIMIT 5
